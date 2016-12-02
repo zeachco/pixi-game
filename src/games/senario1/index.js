651e2717bd4b7f12cda05aaee66cc83b7deae70b
Olivier Rousseau-Guyot <zeachco@gmail.com>
2016-12-01 22:32:20 -0500

    user base64 images and game func

--- src/games/senario1/index.js
+++ src/games/senario1/index.js
@@ -3,50 +3,31 @@ import CatImage from './images/cat.png'
 
 export const senario1 = ({renderer, stage}) => {
     renderer.autoResize = true
-    matchWindowSize();
-    window.addEventListener('resize', matchWindowSize);
+
+    // matchWindowSize(); window.addEventListener('resize', matchWindowSize);
+
     renderer.backgroundColor = 0x061639
 
-    // createTexture(CatImage, 'CatImage') var sprite = PIXI     .Sprite
-    // .fromImage("CatImage");
-
-    const catUrl = '/static/media/cat.78bc2f71.png'
-    var texture = PIXI
-        .Texture
-        .fromImage(catUrl);
-
-    // create a new Sprite using the texture
-    var sprite = new PIXI.Sprite(texture);
-
-    sprite.anchor.x = 0.5;
-    sprite.anchor.y = 0.5;
-
-    // move the sprite to the center of the screen
-    sprite.position.x = 200;
-    sprite.position.y = 150;
+    var sprite = PIXI
+        .Sprite
+        .fromImage(CatImage);
 
     stage.addChild(sprite);
+
+    function game() {
+        sprite.anchor.x = 0.5;
+        sprite.anchor.y = 0.5;
+        sprite.position.x = renderer.width / 2;
+        sprite.position.y = renderer.height / 2;
+        sprite.rotation += 0.1;
+    }
 
     animate();
     function animate() {
         requestAnimationFrame(animate);
-
-        // just for fun, let's rotate mr rabbit a little
-        sprite.rotation += 0.1;
-
-        // render the container
+        game()
+        // just for fun, let's rotate mr rabbit a little render the container
         renderer.render(stage);
-    }
-
-    function createTexture(fromSrc, imageId) {
-        var image = new Image()
-        image.src = fromSrc
-        var myBaseTexture = new PIXI.BaseTexture(image)
-        const texture = new PIXI.Texture(myBaseTexture, imageId)
-        PIXI
-            .Texture
-            .addTextureToCache(texture, imageId)
-        return texture
     }
 
     function matchWindowSize() {
