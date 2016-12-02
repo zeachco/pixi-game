d9099771e1e2b25b7dc004a2432a5d6ec7c3b612
Olivier Rousseau-Guyot <zeachco@gmail.com>
2016-12-01 20:52:23 -0500

    added pixi empty canvas

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+import * as PIXI from 'pixi.js'
+
+var renderer = PIXI.autoDetectRenderer(256, 256);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
