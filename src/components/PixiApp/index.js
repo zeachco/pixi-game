6ed73b782b734e113a4be02a3cb2a0f7176a2b6f
Olivier Rousseau-Guyot <zeachco@gmail.com>
2016-12-01 21:03:27 -0500

    reated canvas component

---
+++ src/components/PixiApp/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const PixiApp = props => (
+    <div>ok</div>
+)
+
+
+
+// import * as PIXI from 'pixi.js'
+
+// var renderer = PIXI.autoDetectRenderer(800, 600);
+
+// //Add the canvas to the HTML document
+// document
+//   .body
+//   .appendChild(renderer.view);
+
+// //Create a container object called the `stage`
+// var stage = new PIXI.Container();
+
+// //Tell the `renderer` to `render` the `stage`
+// renderer.render(stage);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App} from './components';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-import * as PIXI from 'pixi.js'
-
-var renderer = PIXI.autoDetectRenderer(256, 256);
-
-//Add the canvas to the HTML document
-document.body.appendChild(renderer.view);
-
-//Create a container object called the `stage`
-var stage = new PIXI.Container();
-
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+ReactDOM.render(<App/>, document.getElementById('root'));
