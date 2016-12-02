734273b26c42ac48d77aac49a99acce0231d01ee
Olivier Rousseau-Guyot <zeachco@gmail.com>
2016-12-01 22:27:22 -0500

    added crap related to cat

--- src/games/senario1/index.js
+++ src/games/senario1/index.js
@@ -3,23 +3,49 @@ import CatImage from './images/cat.png'
 
 export const senario1 = ({renderer, stage}) => {
     renderer.autoResize = true
-    matchWindowSize()
+    matchWindowSize();
     window.addEventListener('resize', matchWindowSize);
     renderer.backgroundColor = 0x061639
 
-    createTexture(CatImage, 'CatImage')
-    var coolTexture = PIXI
-        .Sprite
-        .fromImage("CatImage");
+    // createTexture(CatImage, 'CatImage') var sprite = PIXI     .Sprite
+    // .fromImage("CatImage");
 
-    renderer.render(stage)
+    const catUrl = '/static/media/cat.78bc2f71.png'
+    var texture = PIXI
+        .Texture
+        .fromImage(catUrl);
+
+    // create a new Sprite using the texture
+    var sprite = new PIXI.Sprite(texture);
+
+    sprite.anchor.x = 0.5;
+    sprite.anchor.y = 0.5;
+
+    // move the sprite to the center of the screen
+    sprite.position.x = 200;
+    sprite.position.y = 150;
+
+    stage.addChild(sprite);
+
+    animate();
+    function animate() {
+        requestAnimationFrame(animate);
+
+        // just for fun, let's rotate mr rabbit a little
+        sprite.rotation += 0.1;
+
+        // render the container
+        renderer.render(stage);
+    }
 
     function createTexture(fromSrc, imageId) {
         var image = new Image()
         image.src = fromSrc
         var myBaseTexture = new PIXI.BaseTexture(image)
         const texture = new PIXI.Texture(myBaseTexture, imageId)
-        PIXI.Texture.addTextureToCache(texture, imageId)
+        PIXI
+            .Texture
+            .addTextureToCache(texture, imageId)
         return texture
     }
 
